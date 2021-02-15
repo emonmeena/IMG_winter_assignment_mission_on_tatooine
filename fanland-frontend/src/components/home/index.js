8e9a3265d6afa744f9ce39cd65f180e1c7313bac
maayami <mayank16meena@gmail.com>
2021-02-16 00:02:51 +0530

    done with the home page layout st-0

--- fanland-frontend/src/components/home/index.js
+++ fanland-frontend/src/components/home/index.js
@@ -1,11 +1,19 @@
 import React from "react";
 import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
+import "./index.css";
+import Club from "./club/club";
 
 const routes = [
   {
     path: "/",
     exact: true,
-    main: () => <h2>Home</h2>,
+    main: () => (
+      <div className="clubs-container">
+        {data.map((data, index) => (
+          <Club key={index} />
+        ))}
+      </div>
+    ),
   },
   {
     path: "/explore",
@@ -20,43 +28,47 @@ const routes = [
 export default function Home() {
   return (
     <Router>
-      <div>
-        <nav style={{backgroundColor: "red"}}> this is nav babes</nav>
-      </div>
-      <div style={{ display: "flex" }}>
-        <div
-          style={{
-            padding: "10px",
-            width: "10%",
-            background: "#f0f0f0",
-          }}
-        >
-          <ul style={{ listStyleType: "none", padding: 0 }}>
-            <li>
-              <Link to="/">Home</Link>
-            </li>
-            <li>
-              <Link to="/explore">Explore</Link>
-            </li>
-            <li>
-              <Link to="/user_admin_clubs">Your clubs</Link>
-            </li>
-          </ul>
+      <div
+        className="main-container text-white"
+        style={{ backgroundColor: "black" }}
+      >
+        <div>
+          <nav> this is nav babes</nav>
         </div>
+        <div className="container-fluid">
+          <div className="row">
+            <div className="col-1 bg-color-secondary">
+              <ul style={{ listStyleType: "none", padding: 0 }}>
+                <li>
+                  <Link to="/">Home</Link>
+                </li>
+                <li>
+                  <Link to="/explore">Explore</Link>
+                </li>
+                <li>
+                  <Link to="/user_admin_clubs">Your clubs</Link>
+                </li>
+              </ul>
+            </div>
 
-        <div style={{ flex: 1, padding: "10px" }}>
-          <Switch>
-            {routes.map((route, index) => (
-              <Route
-                key={index}
-                path={route.path}
-                exact={route.exact}
-                children={<route.main />}
-              />
-            ))}
-          </Switch>
+            <div className className="col-10 bg-color-primary">
+              <Switch>
+                {routes.map((route, index) => (
+                  <Route
+                    key={index}
+                    path={route.path}
+                    exact={route.exact}
+                    children={<route.main />}
+                  />
+                ))}
+              </Switch>
+            </div>
+            <div className="col-1 bg-color-secondary"></div>
+          </div>
         </div>
       </div>
     </Router>
   );
 }
+
+const data = [1, 2, 3, 4, 5, 2,3,3,3,3,3,3,3,3,3,];
